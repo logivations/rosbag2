e8656a57affb11f8e53d22502915e44d64882a07
Johannes Plapp <johannes.plapp@logivations.com>
2025-03-27 22:41:58 +0100

    .

--- rosbag2_cpp/src/rosbag2_cpp/writers/sequential_writer.cpp
+++ rosbag2_cpp/src/rosbag2_cpp/writers/sequential_writer.cpp
@@ -122,7 +122,7 @@ void SequentialWriter::open(
   }
 
   fs::path storage_path(storage_options.uri);
-  if (!storage_path.is_directory()) {
+  if (fs::is_directory(storage_path)) {
       bool dir_created = fs::create_directories(storage_path);
       if (!dir_created) {
         std::stringstream error;
